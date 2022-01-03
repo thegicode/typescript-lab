b1493d4be1faf2062a8e10725d63b79495670438
thegi.code <thegi.code@gmail.com>
2022-01-03 17:20:59 +0900

    Add generic / bounds

---
+++ dist/chapter4/generic-bound.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function filter(array, f) {
+    return [];
+}
+function map(array, f) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result[i] = f(array[i]);
+    }
+    return result;
+}
+function map2(array, f) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result[i] = f(array[i]);
+    }
+    return result;
+}
+//# sourceMappingURL=generic-bound.js.map
