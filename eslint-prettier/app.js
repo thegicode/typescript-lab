f810443013f191c2ff1a62dd81932a53e0bf3608
thegicode <thegi.code@gmail.com>
2022-05-17 12:53:53 +0900

    Add eslint-preiiter

---
+++ eslint-prettier/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+const port = 8000
+
+app.use(express.static(`${__dirname}/static`))
+
+app.listen(port, () => {
+    console.log(`Start : http://localhost:${port}`)
+})
