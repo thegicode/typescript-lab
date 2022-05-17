cb64b94160ed0d6c26a9e0f1da64435d3c361e32
thegicode <thegi.code@gmail.com>
2022-05-17 16:15:59 +0900

    Update eslint-prettier

--- eslint-prettier/src/index.ts
+++ eslint-prettier/src/index.ts
@@ -1,3 +1,10 @@
 import Main from './Main.js'
 
 customElements.define('app-main', Main)
+
+console.log(
+    'rorororfofofofofofoofofofofofofofofofofoofofofofofofoo eoeoeog eogoegoeg eoge goeo geo'
+)
+
+const foo = 'agieigie'
+console.log('egk', foo)
