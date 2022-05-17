6ce45ff38341a64194318ed82958d5813092bfb4
thegicode <thegi.code@gmail.com>
2022-05-17 11:34:35 +0900

    Add eslint-node test

---
+++ eslint-node/src/Main.ts
@@ -0,0 +1,9 @@
+
+const main = () => {
+    console.log('main')
+}
+
+module.exports = main
+
+
+
---
+++ eslint-node/static/Main.js
@@ -0,0 +1,5 @@
+"use strict";
+const main = () => {
+    console.log('main');
+};
+module.exports = main;
