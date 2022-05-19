3c03eb9d6556af9c8c311b6c3df2b0dc964a0fff
thegicode <thegi.code@gmail.com>
2022-05-19 11:31:08 +0900

    Add eslint-prettier-jest

---
+++ eslint-prettier-jest/static/index.js
@@ -0,0 +1,5 @@
+import Main from './Main.js';
+customElements.define('app-main', Main);
+console.log('rorororfofofofofofoofofofofofofofofofofoofofofofofofoo eoeoeog eogoegoeg eoge goeo geo');
+const foo = 'agieigie';
+console.log('egk', foo);
